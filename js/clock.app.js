d01a7edab38eaa61e0e0bb0d3446e65fb0dbe3fe
Luis Gonzalez <luisegr149@gmail.com>
2017-08-17 17:26:37 -0500

    Responsive

--- js/clock.app.js
+++ js/clock.app.js
@@ -1,10 +1,10 @@
-var clockApp = angular.module("Clock", ['ngMaterial']);
+var clockApp = angular.module("Clock", ['ngMaterial', 'mdPickers']);
 clockApp.directive('clock', function(){
   return {
    restrict: 'E',
    template: '<div class="clock" >'+
      '{{actualTime.format("HH:mm:ss")}}'+
    '</div>'+
-   '<br><md-button class="md-raised">Editar</md-button>'
+   '<br><md-button class="md-raised" ng-click="ctrl.showTimePicker($event)">Editar</md-button>'
   };
 });
--- js/clock.controller.js
+++ js/clock.controller.js
@@ -1,8 +1,19 @@
-clockApp.controller("clockController", ["$scope", "$interval", function($scope, $interval){
-  $scope.actualTime = moment();
-
+clockApp.controller("clockController", ["$scope", "$interval", "$mdpTimePicker", function($scope, $interval, $mdpTimePicker){
+  $scope.timeClock = [moment(), moment(), moment(), moment()];
+  $scope.totalClocks = Array(4);
   $interval(function(){
-    $scope.actualTime.subtract(1, 'seconds')
-  }, 1000)
+    $scope.timeClock[0].subtract(1, 'seconds');
+  }, 1000);
+
+
+  $scope.editTime = function(ev, numClock){
+       console.log($scope.timeClock[numClock]);
+    $mdpTimePicker($scope.timeClock[numClock], {
+      targetEvent: ev
+    }).then(function(selectedDate) {
+      console.log(selectedDate);
+      $scope.timeClock[numClock] = moment(selectedDate);
+    });
+  }
 
 }]);
